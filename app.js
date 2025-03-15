9618fcce6a72496a5fba4ab11179a52ee44f336e
justfaahedah <lawalolaitan4407@gmail.com>
2025-03-15 21:49:19 +0100

    variable assignment

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ let day = "thursday"
 
 let position = 4
 
-let output = "today is thursday and it is the forth day of the week" 
+let output = "today is " +  day +   " and it is the "  + position + "th day of the week" 
 
 console.log(output)
-let day1 = "monday"
-
-let position1 = 1
-
-let output1 = "today is monday and it is the first day of the week" 
-
-console.log(output1)
